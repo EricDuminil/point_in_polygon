a37f357780790dd4d2940a405b43f7c70af1af80
Eric Duminil <eric.duminil@gmail.com>
2019-06-21 01:36:18 +0200

    Wrapping parameters isn't needed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace 'v1' do
     get 'areas', to: 'areas#index'
     get 'areas/contain', to: 'areas#contain'
-    #ADD post
   end
   #ADD standard redirect or error message
 end
