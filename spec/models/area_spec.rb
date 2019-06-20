39d43b8c5a687201ad84d8075141b836f97f2cc5
Eric Duminil <eric.duminil@gmail.com>
2019-06-20 17:58:12 +0200

    Area.to_json should export GeoJSON

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe Area, type: :model do
       end
     end
   end
+
+  context '.to_json' do
+    it 'exports GeoJSON' do
+      geo_json = Area.to_json
+      expect(geo_json).to be_a String
+      expect(geo_json).to include "Polygon"
+      expect(geo_json).to include "46.66451741754235"
+      expect(geo_json).to include "13.886718749999998"
+    end
+  end
 end
