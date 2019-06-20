30c69912d42690c41fe2fdad5f8c70d9293835f2
Eric Duminil <eric.duminil@gmail.com>
2019-06-20 18:51:20 +0200

    Area.contains? should check if a given location is included

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
+def geo_json_point(latitude, longitude)
+  %Q|{ "type": "Point", "coordinates": [#{longitude}, #{latitude}] }| # WARNING: Lon/Lat Convention!
+end
+
 RSpec.describe Area, type: :model do
   context '.all' do
     areas = Area.all
@@ -23,6 +27,38 @@ RSpec.describe Area, type: :model do
       expect(geo_json).to include "Polygon"
       expect(geo_json).to include "46.66451741754235"
       expect(geo_json).to include "13.886718749999998"
+    end
+  end
+
+  context '.contains?' do
+    it 'checks if a given location is inside any of the given areas' do
+      expect(Area.contains?(geo_json_point(0, 0))).to eq false
+    end
+
+    inside_points = {
+      knoxville: [35.979643, -83.920342],
+      phoenix: [33.45, -112.066667],
+      lilongwe: [-13.983333, 33.783333],
+      toulouse: [43.602522, 1.429410],
+    }
+
+    inside_points.each do |name, coords|
+      it "checks that #{name.to_s.titleize} is inside" do
+        expect(Area.contains?(geo_json_point(*coords))).to eq true
+      end
+    end
+
+    outside_points = {
+      salt_lake_city: [40.75, -111.883],
+      n_y_c: [40.7128, -74.0060],
+      nairobi: [-1.2833, 36.8167],
+      monaco: [43.736981, 7.421389],
+    }
+
+    outside_points.each do |name, coords|
+      it "checks that #{name.to_s.titleize} is outside" do
+        expect(Area.contains?(geo_json_point(*coords))).to eq false
+      end
     end
   end
 end
