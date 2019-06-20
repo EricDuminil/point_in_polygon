bbddd96a093e8fa13421c5f439a3298def340e36
Eric Duminil <eric.duminil@gmail.com>
2019-06-20 16:57:04 +0200

    Dummy Area class

---
+++ app/models/area.rb
@@ -0,0 +1,5 @@
+class Area
+  def self.all
+    [1, 2, 3, 4, 5, 6]
+  end
+end
--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Area, type: :model do
   context '.all' do
+    areas = Area.all
+
     it 'lists every area' do
-      areas = Area.all
       expect(areas).to be_an Enumerable
+      expect(areas.size).to eq 6
     end
   end
 end
