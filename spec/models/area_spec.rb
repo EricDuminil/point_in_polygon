19af9aa8d6b8252ed32fa3103703f08558ce290a
Eric Duminil <eric.duminil@gmail.com>
2019-06-20 17:20:29 +0200

    Areas should have a geometry

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe Area, type: :model do
       expect(areas).to be_an Enumerable
       expect(areas.size).to eq 6
     end
+
+    it 'contains geometries' do
+      areas.each do |area|
+        expect(area).to respond_to :geometry
+      end
+    end
   end
 end
