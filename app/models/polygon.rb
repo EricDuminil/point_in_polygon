7ff99292c885803a972710c1f96418e82c3d6693
Eric Duminil <eric.duminil@gmail.com>
2019-06-21 22:58:59 +0200

    Very basic check, based on bounding box

--- app/models/polygon.rb
+++ app/models/polygon.rb
@@ -1,9 +1,23 @@
 class Polygon
+  attr_reader :longitude_range, :latitude_range
+  attr_reader :geometry
+
   def initialize(geometry)
     @geometry = geometry
+    calculate_bounding_box
   end
 
   def contains?(point)
-    true
+    #FIXME: Very basic check based on the bounding box. It is only correct for axis-aligned rectangles.
+    longitude_range.include?(point.longitude) &&
+      latitude_range.include?(point.latitude)
+  end
+
+  private
+
+  def calculate_bounding_box
+    lons, lats =  geometry.coordinates.flatten(1).transpose
+    @longitude_range = Range.new(*lons.minmax)
+    @latitude_range = Range.new(*lats.minmax)
   end
 end
