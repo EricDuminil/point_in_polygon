5da58e7926ce8e29f6b685b9afceaa7236cd96af
Eric Duminil <eric.duminil@gmail.com>
2019-06-21 23:04:48 +0200

    Point: Float validation

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,7 +1,7 @@
 class Point
   attr_reader :longitude, :latitude
   def initialize(longitude, latitude)
-    @longitude = longitude
-    @latitude = latitude
+    @longitude = Float(longitude)
+    @latitude = Float(latitude)
   end
 end
